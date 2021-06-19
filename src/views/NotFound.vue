fcd94d0c675385841030cc5c757118dddd141ac8
Javier Estupinan <javier@elnino.tech>
2021-06-19 13:25:38 +0200

    Add 404 page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="w-full">
+    <div class="py-16 w-full bg-yellow text-blue text-center">
+      <div class="mx-auto py-16 max-w-4xl">
+        <div class="font-graduate text-7xl">PAGE NOT FOUND</div>
+        <div class="my-8 text-2xl">Sorry we could not find the page you are looking for, please go back <router-link :to="{ name: 'Home' }" class="text-light-blue">Home</router-link>.</div>
+      </div>
+    </div>
+  </div>
+</template>
