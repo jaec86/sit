00a6c69844afe23b335df69eb619c26b54cbc873
Javier Estupinan <javier@elnino.tech>
2021-06-19 13:16:06 +0200

    Update about view

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="app" class="w-full font-roboto antialiased">
-    <div class="sticky flex justify-center items-center py-8 w-full bg-white shadow">
-      <router-link :to="{ name: 'Home' }" class="mx-8 text-2xl text-blue">Project</router-link>
-      <router-link :to="{ name: 'About' }" class="mx-8 text-2xl text-blue">About Us</router-link>
+  <div id="app" class="w-full minh-full font-roboto antialiased">
+    <div class="sticky top-0 flex justify-center items-center py-8 w-full bg-white shadow z-50">
+      <router-link :to="{ name: 'Home' }" class="mx-8 text-2xl text-blue" active-class="text-yellow" exact>Project</router-link>
+      <router-link :to="{ name: 'About' }" class="mx-8 text-2xl text-blue" active-class="text-yellow" exact>About Us</router-link>
     </div>
     <router-view/>
     <div class="py-16 px-8 bg-blue text-center">
@@ -15,7 +15,7 @@
         <div class="mx-4 p-px h-6 rounded bg-white"></div>
         <div>Angelique van Leeuwen</div>
         <div class="mx-4 p-px h-6 rounded bg-white"></div>
-        <div>Marie-Laurie Snijders</div>
+        <div>Marie-Laure Snijders</div>
         <div class="mx-4 p-px h-6 rounded bg-white"></div>
         <div>Zhirou Sun</div>
       </div>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,52 @@
 <template>
-  <div>
-    <h1>This is an about page</h1>
+  <div class="w-full">
+    <div class="py-12 w-full bg-yellow text-blue text-center">
+      <div class="mx-auto max-w-4xl">
+        <div class="font-graduate text-7xl">About Us</div>
+        <div class="my-8 text-2xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim amet cumque quo at eius nulla, voluptates assumenda saepe, consequatur fuga sapiente. Quas optio adipisci dolor possimus quos, maiores aliquid placeat! nemo ab praesentium voluptatum, exercitationem ad quo tempore quam temporibus voluptates odio, qui laborum pariatur perferendis! Sapiente, itaque facilis deleniti unde?Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+      </div>
+    </div>
+    <div class="py-12 w-full bg-white text-blue text-center">
+      <div class="mx-auto max-w-screen-2xl">
+        <div class="font-graduate text-7xl">Our Members</div>
+        <div class="flex flex-wrap justify-center items-center py-16 w-full">
+          <div class="px-12 py-16 text-2xl">
+            <div class="mx-auto w-48 h-48 rounded-full bg-gray"></div>
+            <div class="mb-4 mt-8 font-graduate text-light-blue text-3xl">Marie-Laure Snijders</div>
+            <div>Biomedical Engineering</div>
+            <div>(S&T faculty)</div>
+            <a href="mailto:m.a.h.c.snijders@student.utwente.nl">m.a.h.c.snijders@student.utwente.nl</a>
+          </div>
+          <div class="px-12 py-16 text-2xl">
+            <div class="mx-auto w-48 h-48 rounded-full bg-gray"></div>
+            <div class="mb-4 mt-8 font-graduate text-light-blue text-3xl">Zhirou Sun</div>
+            <div>Interaction Technology</div>
+            <div>(EMMCS faculty)</div>
+            <a href="mailto:m.a.h.c.snijders@student.utwente.nl">z.sun@student.utwente.nl</a>
+          </div>
+          <div class="px-12 py-16 text-2xl">
+            <div class="mx-auto w-48 h-48 rounded-full bg-gray"></div>
+            <div class="mb-4 mt-8 font-graduate text-light-blue text-3xl">Angelique van Leeuwen</div>
+            <div>Biomedical Engineering</div>
+            <div>(S&T faculty)</div>
+            <a href="mailto:m.a.h.c.snijders@student.utwente.nl">a.a.m.valeeuwen@student.utwente.nl</a>
+          </div>
+          <div class="px-12 py-16 text-2xl">
+            <div class="mx-auto w-48 h-48 rounded-full bg-gray"></div>
+            <div class="mb-4 mt-8 font-graduate text-light-blue text-3xl">Andrea Gaytan</div>
+            <div>Engineering Technology</div>
+            <div>(ET faculty)</div>
+            <a href="mailto:m.a.h.c.snijders@student.utwente.nl">agaytamalvizo@student.utwente.nl</a>
+          </div>
+          <div class="px-12 py-16 text-2xl">
+            <div class="mx-auto w-48 h-48 rounded-full bg-gray"></div>
+            <div class="mb-4 mt-8 font-graduate text-light-blue text-3xl">Javier Estupinan</div>
+            <div>Interaction Technology</div>
+            <div>(EEMCS faculty)</div>
+            <a href="mailto:m.a.h.c.snijders@student.utwente.nl">j.a.estupinancoello@student.utwente.nl</a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
