4478ebe505296d8c60dd1c50acadbca59b7e9419
Javier Estupinan <javier@elnino.tech>
2021-06-24 23:05:34 +0200

    Update home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,17 +17,13 @@
     <div class="py-12 w-full bg-yellow text-blue text-center">
       <div class="mx-auto max-w-4xl">
         <div class="font-graduate text-7xl">About Our Project</div>
-        <div class="my-8 text-2xl">Water polo is a high-intensity sport that demands a lot of strength and intensity. It's a high-impact sport that needs a high level of strategic thinking in addition to physical endurance. In order to improve the performance and decision-making skill of water polo players the “Sharkball Project” was created, which consists of interactive projections on the water surface.</div>
+        <div class="my-8 text-2xl">In order to improve the performance and decision-making skill of water polo players the “Sharkball Project” was created, which consists of interactive projections on the water surface. --> In order to improve the decision-making skill of young water polo players, the “Sharkball Project” was created. Our setup uses interactive visualisations projected on the water surface that create a gamified interaction to support and motivate the players' learning process.</div>
       </div>
     </div>
     <div class="pl-8 pt-16 w-full bg-white">
       <div class="mx-auto max-w-screen-xl pt-8 pl-6 pr-16">
         <div class="relative flex flex-col justify-center items-center">
           <img src="@/assets/ideation.svg" class="w-full" />
-          <!-- <div class="absolute inset-0 bg-black opacity-50"></div>
-          <div class="absolute flex flex-col justify-center items-center w-48 h-48 rounded-full bg-black bg-opacity-75 cursor-pointer">
-            <img src="@/assets/play.svg" class="ml-6" />
-          </div> -->
         </div>
       </div>
     </div>
@@ -44,7 +40,7 @@
           </div>
           <p class="py-2">There is a current need in water polo to develop a skill-targeted sports interaction technology on decision-making.This perceptual-cognitive skill is crucial in the fast-paced game of water polo and can strongly affect the performance. Because this skill is crucial to the sport, it is important that it is developed early in the learning process, such that young players can grow into the sport without becoming daunted by its fast pace and skill-intensive nature. This leads to the following problem statement.</p>
           <p class="py-2 italic">How can we improve the <strong>decision-making skill</strong> of <strong>children</strong> (aged 6-12) learning <strong>water polo</strong> to increase their in-game <strong>performance</strong> and sports <strong>enjoyment</strong>?</p>
-          <p class="py-2">To improve the decision-making skill, an interactive tech-nology needs to be designed which effectively improvesperformance, while having a high task specificity to ensure ahigh transfer from intervention to in-game performance. Ad-ditionally, since the intervention will target children, thereshould be a strong focus on the engagement of the interac-tion such that it provides a motivational aspect to the sport.</p>
+          <p class="py-2">To improve the decision-making skill, an interactive technology needs to be designed which effectively improves performance, while having a high task specificity to ensure a high transfer from intervention to in-game performance. Additionally, since the intervention will target children, there should be a strong focus on the engagement of the interaction such that it provides a motivational aspect to the sport.</p>
         </div>
         <div class="relative py-8">
           <div class="mb-4 ml-4 w-96 h-80 bg-gray float-right">
