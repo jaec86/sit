41df83d812aeb0d2a96d93ccfeb29bd7f3324bd6
Javier Estupinan <javier@elnino.tech>
2021-06-25 13:08:16 +0200

    Update views

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,7 +3,7 @@
     <div class="py-12 w-full bg-yellow text-blue text-center">
       <div class="mx-auto max-w-4xl">
         <div class="font-graduate text-7xl">About Us</div>
-        <div class="my-8 text-2xl">We are five students from the University of Twente (The Netherlands) with an interest in sports and a passion in helping athletes become the best they can be. Our Sharkball project is the result of a multi-disciplinary collaborative focused on designing a new way to help young water polo players to improve their decision-making skills in this exciting but incredibly fast-paced sport. Children can become more familiar with the skills they will later need to understand and apply quick tactical assessments in water polo. Sharkball will help them build confidence in their skills without feeling like they have literally been thrown into the deep end. By using a gamified interaction, our young players will become more comfortable in the water and engage with the sport while increasing their skills. This will prove to be invaluable as they slowly transition from the fun interactions of their ‘mini polo’ to the teen and adult water polo matches.</div>
+        <div class="my-8 text-2xl">We are five students from the University of Twente (The Netherlands) with an interest in sports and a passion for helping athletes become the best they can be. Our Sharkball project is the result of a multi-disciplinary collaboration focused on designing a new way to help young water polo players to improve their decision-making skills in this exciting but incredibly fast-paced sport. Children can become more familiar with the skills they will later need to understand and apply quick tactical assessments in water polo. Sharkball will help them build confidence in their skills without feeling like they have literally been thrown into the deep end. By using a gamified interaction, our young players will become more comfortable in the water and engage with the sport while increasing their skills. This will prove to be invaluable as they slowly transition from the fun interactions of their ‘mini polo’ to the teen and adult water polo matches.</div>
       </div>
     </div>
     <div class="py-12 w-full bg-white text-blue text-center">
@@ -11,35 +11,45 @@
         <div class="font-graduate text-7xl">Our Members</div>
         <div class="flex flex-wrap justify-center items-center py-16 w-full">
           <div class="px-12 py-16 text-2xl">
-            <!-- <div class="mx-auto w-48 h-48 rounded-full bg-gray"></div> -->
+            <div class="mx-auto w-48 h-48 rounded-full bg-gray overflow-hidden">
+              <img src="@/assets/marielaure.jpg" class="w-full h-full object-cover object-center" />
+            </div>
             <div class="mb-4 mt-8 font-graduate text-light-blue text-3xl">Marie-Laure Snijders</div>
             <div>Biomedical Engineering</div>
             <div>(S&T faculty)</div>
             <a href="mailto:m.a.h.c.snijders@student.utwente.nl">m.a.h.c.snijders@student.utwente.nl</a>
           </div>
           <div class="px-12 py-16 text-2xl">
-            <!-- <div class="mx-auto w-48 h-48 rounded-full bg-gray"></div> -->
+            <div class="mx-auto w-48 h-48 rounded-full bg-gray overflow-hidden">
+              <img src="@/assets/zhirou.jpg" class="w-full h-full object-cover object-center" />
+            </div>
             <div class="mb-4 mt-8 font-graduate text-light-blue text-3xl">Zhirou Sun</div>
             <div>Interaction Technology</div>
             <div>(EMMCS faculty)</div>
             <a href="mailto:m.a.h.c.snijders@student.utwente.nl">z.sun@student.utwente.nl</a>
           </div>
           <div class="px-12 py-16 text-2xl">
-            <!-- <div class="mx-auto w-48 h-48 rounded-full bg-gray"></div> -->
+            <div class="mx-auto w-48 h-48 rounded-full bg-gray overflow-hidden">
+              <img src="@/assets/angelique.jpeg" class="w-full h-full object-cover object-center" />
+            </div>
             <div class="mb-4 mt-8 font-graduate text-light-blue text-3xl">Angelique van Leeuwen</div>
             <div>Biomedical Engineering</div>
             <div>(S&T faculty)</div>
             <a href="mailto:m.a.h.c.snijders@student.utwente.nl">a.a.m.valeeuwen@student.utwente.nl</a>
           </div>
           <div class="px-12 py-16 text-2xl">
-            <!-- <div class="mx-auto w-48 h-48 rounded-full bg-gray"></div> -->
+            <div class="mx-auto w-48 h-48 rounded-full bg-gray overflow-hidden">
+              <img src="@/assets/andrea.jpeg" class="w-full h-full object-cover object-center" />
+            </div>
             <div class="mb-4 mt-8 font-graduate text-light-blue text-3xl">Andrea Gaytan</div>
             <div>Engineering Technology</div>
             <div>(ET faculty)</div>
             <a href="mailto:m.a.h.c.snijders@student.utwente.nl">agaytamalvizo@student.utwente.nl</a>
           </div>
           <div class="px-12 py-16 text-2xl">
-            <!-- <div class="mx-auto w-48 h-48 rounded-full bg-gray"></div> -->
+            <div class="mx-auto w-48 h-48 rounded-full bg-gray overflow-hidden">
+              <img src="@/assets/javier.jpeg" class="w-full h-full object-cover object-center" />
+            </div>
             <div class="mb-4 mt-8 font-graduate text-light-blue text-3xl">Javier Estupinan</div>
             <div>Interaction Technology</div>
             <div>(EEMCS faculty)</div>
